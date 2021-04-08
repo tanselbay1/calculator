84869bf8b659ebe9b7b9ae6ed3264a93d6d8d9e1
tanselbay1 <tanselbay1@hotmail.com>
2021-04-08 22:24:20 +0700

    Finished vers.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// FUNCTIONS
+
 function add(num1, num2){
     return num1 + num2;
 }
@@ -10,15 +12,29 @@ function multiply(num1, num2){
     return num1 * num2;
 }
 
+//Used toFixed to prevent number become too long
 function divide(num1, num2){
-    return num1 / num2;
+    if(num2 == 0){
+        alert("Please don't crash your calculator. If something divide to 0 result will be infinity!")
+        return (num1 / (num2+1));
+    }else {
+        return (num1 / num2).toFixed(2);
+    } 
 }
 
 function operate(operator, num1, num2){
     return operator(parseInt(num1), parseInt(num2));
 }
 
-// Variables 
+function clear(){
+    displayValue = "";
+    secondValue = "";
+    operation = "";
+    firstNumEntered = false;
+    screen.innerText = "Ready..";
+}
+
+// VARIABLES 
 
 let displayValue = "";
 let secondValue = "";
@@ -28,12 +44,15 @@ let firstNumEntered = false;
 let numbers = document.querySelectorAll(".number");
 let screen = document.getElementById("screen");
 
-let plusButton = document.getElementById("plus");
-let minusButton = document.getElementById("minus");
-let multipyButton = document.getElementById("multiply");
-let divideButton = document.getElementById("divide");
+const plusButton = document.getElementById("plus");
+const minusButton = document.getElementById("minus");
+const multiplyButton = document.getElementById("multiply");
+const divideButton = document.getElementById("divide");
 
-let sumButton = document.getElementById('sum');
+const sumButton = document.getElementById('sum');
+const clearButton = document.getElementById('clear');
+
+// EVENTS
 
 Array.from(numbers).map(number => {
     number.addEventListener('click', () => {
@@ -55,19 +74,55 @@ Array.from(numbers).map(number => {
 
 plusButton.addEventListener('click', () => {
     firstNumEntered = true;
-    operation = 'adding';
+    operation = 'addition';
     screen.innerText += ` + `;
+})
+
+minusButton.addEventListener('click', () => {
+    firstNumEntered = true;
+    operation = 'subtraction';
+    screen.innerText += ` - `;
+})
+
+multiplyButton.addEventListener('click', () => {
+    firstNumEntered = true;
+    operation = 'multiplication';
+    screen.innerText += ` x `;
+})
+
+divideButton.addEventListener('click', () => {
+    firstNumEntered = true;
+    operation = 'division';
+    screen.innerText += ` ÷ `;
 })
 
 sumButton.addEventListener('click', () => {
     let operated;
     switch(operation){
-        case('adding'):
-            console.log('adding');
+        case 'addition':
+            console.log('addition');
             operated = operate(add, displayValue, secondValue);
+            screen.innerText += ` = ${operated}`;
+            break;
+        case 'subtraction':
+            console.log('subtraction');
+            operated = operate(subtract, displayValue, secondValue);
+            screen.innerText += ` = ${operated}`;
+            break;
+        case 'multiplication':
+            console.log('multiplication');
+            operated = operate(multiply, displayValue, secondValue);
+            screen.innerText += ` = ${operated}`;
+            break;
+        case 'division':
+            console.log('division');
+            operated = operate(divide, displayValue, secondValue);
             screen.innerText += ` = ${operated}`;
             break;
         default:
             console.log('not working!');
     }
+    setTimeout(clear, 3000);
 })
+
+clearButton.addEventListener('click', clear);
